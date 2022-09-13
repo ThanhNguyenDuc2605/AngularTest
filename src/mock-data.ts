07e5c11b171fd8bacab6db81c21f7a09f8f08858
Thanh Nguyen Duc <thanh.nguyenduc@powergatesoftware.com>
2022-09-13 19:08:38 +0700

    add all

---
+++ src/mock-data.ts
@@ -0,0 +1,8 @@
+export const ListQuestion = [
+    { question: 'Please tell us about yourself', type: "paragraph", otherAnswer: null, require: true, questionContent: "" },
+    {
+        question: 'Please select the languages you know', type: "checkbox", otherAnswer: true, arrayAnswers: ["Typescript",
+            "Python", "C#"], require: true
+    },
+
+];
